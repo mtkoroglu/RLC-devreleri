12e795689c99cf6f6783e534785567fab307fa33
mtkoroglu <m.tahakoroglu@gmail.com>
2022-03-25 00:14:25 +0300

    Add files via upload

--- kodlar/RC_zorlanmis_cevap.m
+++ kodlar/RC_zorlanmis_cevap.m
@@ -1,5 +1,5 @@
 clear all; close all; clc;
-T = 1e-3; stopTime = 2; % saniye
+T = 1e-2; stopTime = 2; % saniye
 R1 = 1e4; % 10k
 C1 = 1e-5; % 10uF
 R2 = 2e4; C2 = C1;
@@ -38,7 +38,59 @@ leg3 = sprintf('R = %ik\\Omega  x  C = %i\\muF  \\rightarrow  \\tau = %.2f', s1*
 leg4 = sprintf('R = %ik\\Omega  x  C = %.1f\\muF  \\rightarrow  \\tau = %.2f', s1*R4, s2*C4, tau(4));
 leg5 = sprintf('R = %.1fk\\Omega  x  C = %i\\muF  \\rightarrow  \\tau = %.2f', s1*R5, s2*C5, tau(5));
 legend(leg1, leg2, leg3, leg4, leg5, 'location', 'southeast');
-set(legend, 'Interpreter', 'tex');
-grid on;
-set(gca, 'gridlinestyle', '--');
+set(legend, 'Interpreter', 'tex', 'fontsize', 12);
+grid on; set(gca, 'gridlinestyle', '--');
 axis([0 2 -0.2 5.4]);
+%%
+figure(2);
+lw = 1.1;
+generateGif = false;
+generateVideo = true;
+if generateVideo
+    v = VideoWriter('RC_circuit_step_response.mp4','MPEG-4');
+    v.Quality = 100; v.FrameRate = 30;
+    open(v);
+end
+for i=1:length(t)
+    cla;
+    plot(t(1:i), Vc1(1:i), 'k-', 'linewidth', lw);
+    hold on;
+    plot(t(1:i), Vc2(1:i), 'r-', 'linewidth', lw);
+    plot(t(1:i), Vc3(1:i), 'g-', 'linewidth', lw);
+    plot(t(1:i), Vc4(1:i), 'm-', 'linewidth', lw);
+    plot(t(1:i), Vc5(1:i), 'b-', 'linewidth', lw);
+    hold off;
+    set(gca, 'position', [0.0918 0.1062 0.8929 0.8438]);
+    xlabel('Zaman (s)');
+    ylabel('V_C(t) (Volt)');
+    title('RC devresinin zorlanmış (basamak) cevabı', 'fontweight', 'normal');
+    s1 = 1e-3; % scale R values to print them so we have units in kohm
+    s2 = 1e6; % % scale C values to print them so we have units in uF
+    leg1 = sprintf('R = %ik\\Omega  x  C = %i\\muF  \\rightarrow  \\tau = %.2f', s1*R1, s2*C1, tau(1));
+    leg2 = sprintf('R = %ik\\Omega  x  C = %i\\muF  \\rightarrow  \\tau = %.2f', s1*R2, s2*C2, tau(2));
+    leg3 = sprintf('R = %ik\\Omega  x  C = %i\\muF  \\rightarrow  \\tau = %.2f', s1*R3, s2*C3, tau(3));
+    leg4 = sprintf('R = %ik\\Omega  x  C = %.1f\\muF  \\rightarrow  \\tau = %.2f', s1*R4, s2*C4, tau(4));
+    leg5 = sprintf('R = %.1fk\\Omega  x  C = %i\\muF  \\rightarrow  \\tau = %.2f', s1*R5, s2*C5, tau(5));
+    legend(leg1, leg2, leg3, leg4, leg5, 'location', 'southeast');
+    set(legend, 'Interpreter', 'tex', 'fontsize', 12);
+    grid on; set(gca, 'gridlinestyle', '--');
+    axis([0 2 -0.2 5.4]);
+    % make gif animation
+    frame = getframe(gcf);
+    img = frame2im(frame);
+    if generateGif
+        [img, cmap] = rgb2ind(img,256);
+        if i == 1
+            imwrite(img, cmap, 'RC_circuit_step_response.gif', 'gif', 'LoopCount', Inf, 'DelayTime', 0.01);
+        else
+            imwrite(img, cmap, 'RC_circuit_step_response.gif', 'gif', 'WriteMode', 'append', 'DelayTime', 0.01);
+        end
+    end
+    if generateVideo
+        writeVideo(v, img);
+    end
+    %     pause(0.01);
+end
+if generateVideo
+    close(v);
+end
